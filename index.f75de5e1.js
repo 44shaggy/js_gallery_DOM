06d7a64488a36cf4722287e828c742d255d092a2
Emil Sukiennik <ewilemil@gmail.com>
2024-04-06 20:55:04 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const largeImg = document.querySelector("#largeImg");
+const list = document.querySelector(".gallery__list");
+list.addEventListener("click", (listener)=>{
+    listener.preventDefault();
+    const properLink = listener.target.closest("a");
+    largeImg.src = properLink.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
